d2bdfd35c5be909ae30709c85720bc3ee372a943
Felipe Uchoa <femeuf5@gmail.com>
2021-01-01 16:25:11 -0300

    Testando na SampleScene como fazer a câmera girar ao redor do jogador. Consegui fazer isso da seguinte forma: ao clicar e arrastar com o botão do mouse, no caso de um Desktop, ou ao pressionar o dedo na tela e arrastar, no caso de um smartphone.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    #region Class Properties
+
+    public Transform targetOfCamera;
+
+    private Vector3 previousMousePosition;
+
+    public float cameraHeightRelativeToTheTarget = 1.5f;
+    [Tooltip("The distance must be a negative number!")]
+    public float cameraDistanceOfTheTarget = -4;
+
+    private Camera cam;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // LateUpdate is called after all Update functions have been called. 
+    //This is useful to order script execution. For example a follow camera should always 
+    //be implemented in LateUpdate because it tracks objects that might have moved inside Update.
+    void LateUpdate()
+    {
+        cam.transform.position = targetOfCamera.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            // ScreenToViewportPoint(Vector3 position); Transforms position from screen space into viewport space.
+            // Screenspace is defined in pixels. The bottom-left of the screen is (0,0); the right-top is (pixelWidth,pixelHeight).
+            // Viewport space is relative to the camera. The bottom-left of the camera is (0, 0); the top-right is (1, 1). 
+            previousMousePosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0)) // GetMouseButton(int button) Returns whether the given mouse button is held down.
+        {
+            Vector3 rotationDirection = previousMousePosition - cam.ScreenToViewportPoint(Input.mousePosition);
+
+            cam.transform.Rotate(new Vector3(1, 0, 0), rotationDirection.y * 180);
+            cam.transform.Rotate(new Vector3(0, 1, 0), -rotationDirection.x * 180, Space.World);
+
+            previousMousePosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        // Translate(Vector3 translation); Moves the transform in the direction and distance of translation.
+        cam.transform.Translate(0, cameraHeightRelativeToTheTarget, cameraDistanceOfTheTarget);
+    }
+    #endregion
+}   
---
+++ Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    private Rigidbody player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Rigidbody>();
+    }
+
+    public void Throw()
+    {
+        player.AddForce(new Vector3(0, 2, 2), ForceMode.VelocityChange);
+    }
+}
