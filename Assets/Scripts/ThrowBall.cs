1598237deb27dcc3c16b225d2fbac85db4c71b10
Felipe Uchoa <femeuf5@gmail.com>
2021-01-01 17:44:49 -0300

    Testando como fazer a bola ser lançada na direção que a câmera aponta. Além disso, quando a bola atinge uma certa velocidade, o código força a bola a parar para evitar que os jogadres que tenham que esperar até ela parar.

--- Assets/Scripts/ThrowBall.cs
+++ Assets/Scripts/ThrowBall.cs
@@ -2,15 +2,34 @@
 
 public class ThrowBall : MonoBehaviour
 {
+    #region Properties
     private Rigidbody player;
+    private Camera cam;
+    #endregion
+
+    #region MonoBehaviour CallBacks
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<Rigidbody>();
+        cam = Camera.main;
     }
+
+    private void Update()
+    {
+        // Stops the ball when it gets slower than a certain velocity, this way the player doesn't have to 
+        // wait until the ball stops.
+        if(player.velocity.magnitude < .3)
+        {
+            player.velocity = Vector3.zero;
+        }
+    }
+    #endregion
 
     public void Throw()
     {
-        player.AddForce(new Vector3(0, 2, 2), ForceMode.VelocityChange);
+        // transform.forward = Returns a normalized vector representing the blue axis of the transform in world space.
+        // This way the force will be added to the z direction of the camera, which is forward
+        player.AddForce(cam.transform.forward * 500 + new Vector3(0, 1000, 0), ForceMode.Force);
     }
 }
