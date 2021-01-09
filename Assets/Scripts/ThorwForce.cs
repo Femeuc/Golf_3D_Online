4d5b8690d9b0eb352ec77aa7daa7a97366b6f82b
Felipe Uchoa <femeuf5@gmail.com>
2021-01-09 14:05:59 -0300

    Script responsável por atualizar as informações que aparecem na UI sobre as forças para frente e para cima.

---
+++ Assets/Scripts/ThorwForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.Femeuc.Golf3DOnline
+{
+    public class ThorwForce : MonoBehaviour
+    {
+        private Text forwardForceValue, upwardForceValue;
+        // Start is called before the first frame update
+        void Start()
+        {
+            forwardForceValue = GameObject.Find("Forward Force Value").GetComponent<Text>();
+            upwardForceValue = GameObject.Find("Upward Force Value").GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            forwardForceValue.text = (ThrowBall.forwardForceIntensity.value * 10).ToString("0");
+            upwardForceValue.text = (ThrowBall.upwardForceIntensity.value * 10).ToString("0");
+        }
+    }
+}
