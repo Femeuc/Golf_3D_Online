41fddb928a00af2715ccd827f17052eae1880081
Felipe Uchoa <femeuf5@gmail.com>
2021-01-09 14:03:55 -0300

    Atualizei o código para reconhecer os novos elementos gráficos adicionados na scene. Criei uma função personalizada para lidar com o atrito. E dividi o lançamento da bola em lançamento para frente e lançamento para cima.

--- Assets/Scripts/ThrowBall.cs
+++ Assets/Scripts/ThrowBall.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
+using System;
 
 namespace Com.Femeuc.Golf3DOnline
 {
@@ -9,25 +11,32 @@ namespace Com.Femeuc.Golf3DOnline
     {
         private static Rigidbody playerRigidBody;
         private Camera cam;
-        bool isBeingThrown;
+        public static Scrollbar forwardForceIntensity, upwardForceIntensity;
+        
 
         // Start is called before the first frame update
-        void Start()
+        void Start()    
         {
             cam = Camera.main;
+            forwardForceIntensity = GameObject.Find("Forward Force Intensity").GetComponent<Scrollbar>();
+            upwardForceIntensity = GameObject.Find("Upward Force Intensity").GetComponent<Scrollbar>();
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
-            stopWhenGoingTooSlowAfterBeingThrown();
-        }
+            causeFriction(); // Method responsible for causing the friction between ball and ground.
+        }                    // I prefer making my own function than using Rigidbody drag, cause it doesn't look good to me.
 
         public void Throw()
         {
             Debug.Log("Throwing " + PhotonNetwork.LocalPlayer.NickName);
-            playerRigidBody.velocity = cam.transform.forward * 15 + Vector3.up * 5;
-            isBeingThrown = true;
+            int forwardThrowValue = int.Parse((forwardForceIntensity.value * 10).ToString("0"));
+            int upwardThrowValue = int.Parse((upwardForceIntensity.value * 10).ToString("0"));
+            // Quarternion.Euler is necessary so the ball is thorwn only forward, not upward, even if the camera is pointing upward.
+            playerRigidBody.velocity = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0) * Vector3.forward * forwardThrowValue * forwardThrowValue / 3 + Vector3.up * upwardThrowValue * upwardThrowValue / 3;
+            Vector3 v3 = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0) * Vector3.forward;
+
         }
 
         public static void InitializePlayerRigidBody()
@@ -35,16 +44,38 @@ namespace Com.Femeuc.Golf3DOnline
             playerRigidBody = CameraFollow.playerObject.GetComponent<Rigidbody>();
         }
 
-        // This method stops the ball if it going too slow,
-        // this way you don't have to wait until the ball stops
-        private void stopWhenGoingTooSlowAfterBeingThrown()
+        // This function causes friction while the ball is rolling on the ground.
+        private void causeFriction()
         {
-            if (!isBeingThrown) return;
-            if (!(playerRigidBody.velocity.magnitude < 1)) return;
-            Debug.Log("Stoping the ball");
-            playerRigidBody.velocity = Vector3.zero;
-            playerRigidBody.Sleep(); // This is necessary to make the ball stop altogether
-            isBeingThrown = false;
+            if(playerRigidBody == null)
+            {
+                Debug.Log("Rigidbody null: leaving");
+                return;
+            }
+            float baseFramerate = Time.deltaTime * 60;
+            float magnitude = playerRigidBody.velocity.magnitude;
+            if (magnitude > 0 && playerRigidBody.velocity.y == 0) // only if ball is touching ground
+            {
+                // Notice the amount of friction is dependant on the velocity.
+                if (magnitude > 30)
+                    playerRigidBody.AddForce(playerRigidBody.velocity * -0.025f * baseFramerate);
+                else if (magnitude > 10)
+                    playerRigidBody.AddForce(playerRigidBody.velocity * -0.05f * baseFramerate);
+                else if(magnitude > 1)
+                    playerRigidBody.AddForce(playerRigidBody.velocity * -0.15f * baseFramerate);
+                else if (magnitude > 0.5)
+                    playerRigidBody.AddForce(playerRigidBody.velocity * -0.5f * baseFramerate);
+                else
+                {
+                    playerRigidBody.velocity = Vector3.zero;
+                    playerRigidBody.Sleep(); // This is necessary to make the ball stop altogether
+                }
+                Debug.Log(playerRigidBody.velocity.magnitude);
+            }
+            if(magnitude == 0)
+            {
+                Debug.Log("PARADO!");
+            }
         }
     }
 }
