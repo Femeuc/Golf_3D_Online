04d6c46e53f6811888ceaef4d9400c42653f2399
Felipe Uchoa <femeuf5@gmail.com>
2021-01-15 18:27:05 -0300

    Classe para lidar com as colisões da bola.

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Com.Femeuc.Golf3DOnline
+{
+    public class PlayerCollision : MonoBehaviour
+    {
+        public static bool isTouchingTheGround;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.tag == "Ground")
+                isTouchingTheGround = true;
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.collider.tag == "Ground")
+                isTouchingTheGround = false;
+        }
+    }
+}
